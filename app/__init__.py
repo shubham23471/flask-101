771a8e6ad31f32276aaf22cf4863c91059568bef
shubham <shubhamshakya61@gmail.com>
2024-10-16 03:29:58 +0530

    ch3: added form and login view funtion, html render code

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
+from config import Config
 
 # set to the name of the module
 app = Flask(__name__)
-
+app.config.from_object(Config)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app  
+from app.forms import LoginForm
 
 # this is called as view func: handlers for application routes.
 # this decorator create an association b/w URL and the function. 
@@ -18,3 +19,9 @@ def index():
     ]
 	return render_template('index.html', title='Home', user=user,
 							posts=posts)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	# title: the page title (shown on tab)
+	return render_template('login.html', title='Sign in', form=form)
