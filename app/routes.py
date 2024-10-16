c5d464ab06bbd15f8468b2291a35e72a734daa08
shubham <shubhamshakya61@gmail.com>
2024-10-16 12:10:21 +0530

    ch3: added error message in forms, render flash message

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app  
 from app.forms import LoginForm
 
@@ -17,11 +17,17 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
+	# title: the page title (shown on tab)
 	return render_template('index.html', title='Home', user=user,
 							posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
 	form = LoginForm()
-	# title: the page title (shown on tab)
+	 # validate_on_submit(): all the form processing work
+	 # Return False for GET request
+	 # Return: after gathering all the data and if it pass the validators
+	if form.validate_on_submit():
+		flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign in', form=form)
