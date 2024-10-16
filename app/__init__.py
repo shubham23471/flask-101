35730f6b3725e9f75d455f93f783ef759cdd186a
shubham <shubhamshakya61@gmail.com>
2024-10-17 02:46:48 +0530

    ch3: created user table using migration script

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask 
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # set to the name of the module
 app = Flask(__name__)
 app.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(app) # db object 
+migrate = Migrate(app, db) # databse migration engine
+
+from app import routes, models
